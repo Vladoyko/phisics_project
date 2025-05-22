71aff25c1da6e0ccab7ef00d3cbb97616694c125
Vladoyko <166605430+Vladoyko@users.noreply.github.com>
2025-05-22 09:32:10 +0300

    Add files via upload

---
+++ project.py
@@ -0,0 +1,14 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route("/", methods = ["GET","POST"])
+def main():
+    return flask.render_template("index.html")
+
+@app.route("/download",methods = ["GET","POST"])
+def download():
+    return flask.send_file("Simulator.zip",as_attachment=True)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
