76d787fa230b09e50e19b2f1a7ce27947b733827
Vladoyko <166605430+Vladoyko@users.noreply.github.com>
2025-05-29 18:39:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
 
 @app.route("/download",methods = ["GET","POST"])
 def download():
-    return flask.send_file("Simulator.zip",as_attachment=True)
+    return flask.send_file("Simulator.7z",as_attachment=True)
     
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=8000)
